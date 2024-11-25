5152d6b7f7bef3602364ffe3f5e65956d5632c3e
robinAZERTY <robinlecouffe@gmail.com>
2024-11-25 17:04:13 +0100

    fix relative include path

--- src/walle-app/main.cpp
+++ src/walle-app/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <walle-lib/vector2.hpp>
+#include "../walle-lib/vector2.hpp"
 
-#include <walle-lib/robot.hpp>
+#include "../walle-lib/robot.hpp"
 
 /// Les unités sont en mètre,
 int main(int argc, char** argv)

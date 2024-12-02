2137bc9d496df6f820774e99a25fb3c1e148d55c
robinAZERTY <robinlecouffe@gmail.com>
2024-12-02 21:36:46 +0100

    initialization values and improve code readability

--- src/walle-lib/element.hpp
+++ src/walle-lib/element.hpp
@@ -4,7 +4,7 @@ class Environment;
 
 class Element {
     public:
-        friend class Environment;
+        friend class Environment; // Environment needs to access the protected members of Element
 
         Element(Environment *environment = nullptr) : environment_(environment) {}
         Element(double x, double y, Environment *environment = nullptr) : x_(x), y_(y), environment_(environment) {}
@@ -12,7 +12,8 @@ class Element {
         double get_position_x() const { return x_; }
         double get_position_y() const { return y_; }
         Environment *get_environment() const { return environment_; }
+    
     protected :
-        double x_, y_, collision_radius_;
-        Environment *environment_ = nullptr;
+        double x_{0}, y_{0}, collision_radius_{0};
+        Environment *environment_{nullptr};
 };

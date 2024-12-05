696e20689cabb803f14a0314bdbc949e8f048e2b
robinAZERTY <robinlecouffe@gmail.com>
2024-12-05 20:40:52 +0100

    forward declaration instead of include

--- src/walle-lib/environnement.cpp
+++ src/walle-lib/environnement.cpp
@@ -1,5 +1,7 @@
 #include "environnement.hpp"
+#include "element.hpp"
 #include <stdexcept>
+
 
 Environment::Environment(double left, double right, double top, double bottom)
 {
--- src/walle-lib/environnement.hpp
+++ src/walle-lib/environnement.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
-#include "element.hpp"
+
+class Element;
 
 class Environment
 {

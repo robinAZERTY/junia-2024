c24afb52a608fe3670572bd3ae21713de0514db3
robinAZERTY <robinlecouffe@gmail.com>
2024-12-06 09:48:51 +0100

    move implementations to a new element.cpp

---
+++ src/walle-lib/element.cpp
@@ -0,0 +1,33 @@
+#include "element.hpp"
+
+Element::Element(Environment *environment) : environment_(environment) {}
+Element::Element(double x, double y, Environment *environment) : position_(x,y), environment_(environment) {}
+
+double Element::get_position_x() const { return position_.x(); }
+double Element::get_position_y() const { return position_.y(); }
+Vector2<double> Element::get_position() const { return position_; }
+void Element::set_position(double x, double y) { position_ = {x, y}; }
+void Element::set_position(Vector2<double> position) { position_ = position; }
+
+double Element::get_orientation() const { return orientation_; }
+void Element::set_orientation(double orientation) { orientation_ = orientation; }
+
+Environment *Element::get_environment() const { return environment_; }
+void Element::set_environment(Environment *environment) { environment_ = environment; }
+
+FixedElement::FixedElement(Environment *environment) : Element(environment) {}
+FixedElement::FixedElement(double x, double y, Environment *environment) : Element(x, y, environment) {}
+
+MovableElement::MovableElement(Environment *environment) : Element(environment) {}
+MovableElement::MovableElement(double x, double y, Environment *environment) : Element(x, y, environment) {}
+
+double MovableElement::get_linear_speed() const { return linear_speed_; }
+void MovableElement::set_linear_speed(double linear_speed) { linear_speed_ = linear_speed; }
+
+double MovableElement::get_angular_speed() const { return angular_speed_; }
+void MovableElement::set_angular_speed(double angular_speed) { angular_speed_ = angular_speed; }
+
+void MovableElement::move_according_velocities(double dt) {
+    orientation_ += angular_speed_ * dt;
+    position_ += Vector2<double>{cos(orientation_), sin(orientation_)} * linear_speed_ * dt;
+}
--- src/walle-lib/element.hpp
+++ src/walle-lib/element.hpp
@@ -6,20 +6,20 @@ class Environment;
 class Element {
     public:
 
-        Element(Environment *environment = nullptr) : environment_(environment) {}
-        Element(double x, double y, Environment *environment = nullptr) : position_(x,y), environment_(environment) {}
+        Element(Environment *environment = nullptr);
+        Element(double x, double y, Environment *environment = nullptr);
 
-        double get_position_x() const { return position_.x(); }
-        double get_position_y() const { return position_.y(); }
-        Vector2<double> get_position() const { return position_; }
-        void set_position(double x, double y) { position_ = {x, y}; }
-        void set_position(Vector2<double> position) { position_ = position; }
+        double get_position_x() const;
+        double get_position_y() const;
+        Vector2<double> get_position() const;
+        void set_position(double x, double y);
+        void set_position(Vector2<double> position);
 
-        double get_orientation() const { return orientation_; }
-        void set_orientation(double orientation) { orientation_ = orientation; }
+        double get_orientation() const;
+        void set_orientation(double orientation);
 
-        Environment *get_environment() const { return environment_; }
-        void set_environment(Environment *environment) { environment_ = environment; }
+        Environment *get_environment() const;
+        void set_environment(Environment *environment);
     
     protected :
         Vector2<double> position_{0, 0};
@@ -30,26 +30,22 @@ class Element {
 
 class FixedElement : public Element {
     public:
-        FixedElement(Environment *environment = nullptr) : Element(environment) {}
-        FixedElement(double x, double y, Environment *environment = nullptr) : Element(x, y, environment) {}
+        FixedElement(Environment *environment = nullptr);
+        FixedElement(double x, double y, Environment *environment = nullptr);
 };
 
 class MovableElement : public Element {
     public:
-        MovableElement(Environment *environment = nullptr) : Element(environment) {}
-        MovableElement(double x, double y, Environment *environment = nullptr) : Element(x, y, environment) {}
+        MovableElement(Environment *environment = nullptr);
+        MovableElement(double x, double y, Environment *environment = nullptr);
 
-        double get_linear_speed() const { return linear_speed_; }
-        void set_linear_speed(double linear_speed) { linear_speed_ = linear_speed; }
+        double get_linear_speed() const;
+        void set_linear_speed(double linear_speed);
 
-        double get_angular_speed() const { return angular_speed_; }
-        void set_angular_speed(double angular_speed) { angular_speed_ = angular_speed; }
-        
+        double get_angular_speed() const;
+        void set_angular_speed(double angular_speed);
 
-        void move_according_velocities(double dt) {
-            orientation_ += angular_speed_ * dt;
-            position_ += Vector2<double>{cos(orientation_), sin(orientation_)} * linear_speed_ * dt;
-        }
+        void move_according_velocities(double dt);
 
     private:
         double linear_speed_{0};

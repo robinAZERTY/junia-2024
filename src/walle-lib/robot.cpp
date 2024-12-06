cf3643d87b581f102dce4d091142621f3b7c18a8
robinAZERTY <robinlecouffe@gmail.com>
2024-12-06 11:05:58 +0100

    implement battery management in Robot class with consumption tracking and status reporting

--- src/walle-lib/robot.cpp
+++ src/walle-lib/robot.cpp
@@ -9,7 +9,31 @@ void Robot::stop()
     set_angular_speed(0);
 }
 
-void Robot::run(double dt)
+int Robot::run(double dt)
 {
+    auto battery_status = update_battery(dt);
     move_according_velocities(dt);
+    
+    if (battery_status == Robot::BATTERY_STATUS::EMPTY)
+        return Robot::STATUS::EMPTY_BATTERY;
+    if (battery_status == Robot::BATTERY_STATUS::LOW)
+        return Robot::STATUS::LOW_BATTERY;
+    
+    return 0;
+}
+
+int Robot::update_battery(double dt)
+{
+    battery_ -= RobotEnergyProperties::MINIMUM_POWER_CONSUMPTION * dt;
+    battery_ -= RobotEnergyProperties::MOVING_POWER_CONSUMPTION * get_linear_speed() * get_mass() * dt;
+
+    if (battery_ <= 0)
+    {
+        stop();
+        return Robot::BATTERY_STATUS::EMPTY;
+    }
+    if (battery_ < RobotEnergyProperties::LOW_BATTERY_THRESHOLD * battery_capacity_)
+        return Robot::BATTERY_STATUS::LOW;
+
+    return Robot::BATTERY_STATUS::OK;
 }
--- src/walle-lib/robot.hpp
+++ src/walle-lib/robot.hpp
@@ -3,18 +3,38 @@
 #include "environnement.hpp"
 #include "element.hpp"
 
+namespace RobotEnergyProperties {
+    constexpr double MINIMUM_POWER_CONSUMPTION = 5; // W -> the robot consumes at least 5W for its electronic components
+    constexpr double MOVING_POWER_CONSUMPTION = 10; // W/m/s/kg -> the robot consumes 10W per second for each m/s of linear speed and for each kg of mass
+    constexpr double LOW_BATTERY_THRESHOLD = 0.1; // ratio
+}
+
 /// Generic robot class inheriting from MovableElement (can move in the environment).
 class Robot : public MovableElement
 {
 public:
-    Robot(Environment* e=nullptr) : MovableElement(e) {}
+    Robot(Environment* e=nullptr, double mass=10) : MovableElement(e) {set_mass(mass); }
 
     /// Stops the robot.
     void stop();
 
+    enum STATUS { OK, LOW_BATTERY, EMPTY_BATTERY };
     /// update the robot's position and orientation according to its linear and angular speeds.
-    void run(double dt);
+    int run(double dt);
+
+    bool get_battery_ratio() const { return battery_ / battery_capacity_; }
+
+    enum BATTERY_STATUS { OK, LOW, EMPTY };
+private:
+    int update_battery(double dt);
+
+    double battery_capacity_ = 1e5; // J
+    double battery_ = battery_capacity_;
 
 };
 
-
+// need to implement the following classes:
+// class DiggingRobot : public Robot
+// class PantingRobot : public Robot
+// class SoilCovererRobot : public Robot
+// class WateringRobot : public Robot

45e71fcabec63a7f56912632a8524af6e4d3593f
robinAZERTY <robinlecouffe@gmail.com>
2024-12-07 14:57:41 +0100

    update collision radius check in set_element_inside method for proper fitting

--- src/walle-lib/hole.cpp
+++ src/walle-lib/hole.cpp
@@ -16,7 +16,7 @@ int Hole::set_element_inside(Element *element_inside)
     {
         if (element_inside_ != nullptr)
             return -1; // already occupied 
-        if (element_inside->get_collision_radius() > get_collision_radius())
+        if (element_inside->get_collision_radius()/2 > get_size())
             return -2; // don't fit
         
         element_inside->set_position(get_position());
